736fd41816e7db9978397432fc2675ba1ab1d1ff
Leonardo Guzi <leoguzi@hotmail.com>
2024-06-28 14:36:00 -0300

    Feat: Add methods and validations to create new processes

--- paging-simulator.c
+++ paging-simulator.c
@@ -1,6 +1,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <textstyle/stdbool.h>
+#include <string.h>
+
+typedef struct
+{
+    int *pageTable;
+    int processSize;
+    int numPages;
+} PageTable;
+
+typedef struct
+{
+    unsigned char *memory;
+    bool *freeFrames;
+    int totalFrames;
+    int frameSize;
+    int freeFrameCount;
+} PhysicalMemory;
+
+typedef struct
+{
+    int processId;
+    int size;
+    PageTable *pageTable;
+} Process;
 
 int readMultipleOfTwo(char *message)
 {
@@ -25,34 +51,226 @@ int checkPageSizeIsValid(int pageSize, int memorySize)
     return pageSize;
 }
 
-void showMenu()
+void initPhysicalMemory(PhysicalMemory *pm, int memorySize, int frameSize)
 {
-    int option;
+    pm->memory = (unsigned char *)malloc(memorySize * sizeof(unsigned char));
+    pm->frameSize = frameSize;
+    pm->totalFrames = memorySize / frameSize;
+    pm->freeFrames = (bool *)malloc(pm->totalFrames * sizeof(bool));
+    pm->freeFrameCount = pm->totalFrames;
 
-    printf("1 - Create a new process.\n");
-    printf("2 - Show memory status.\n");
-    printf("3 - Show pages table.\n");
-
-    printf("\nType the number of the action and press enter: ");
-    scanf("%d", &option);
-
-    if (option < 1 && option > 2)
+    for (int i = 0; i < pm->totalFrames; i++)
     {
-        printf("Invalid option, try again!\n");
-        return showMenu();
+        pm->freeFrames[i] = true;
     }
 
-    switch (option)
+    memset(pm->memory, 0, memorySize);
+}
+
+void freePhysicalMemory(PhysicalMemory *pm)
+{
+    free(pm->memory);
+    free(pm->freeFrames);
+    printf("Physical memory freed\n");
+}
+
+void viewPhysicaMemory(PhysicalMemory *pm)
+{
+    printf("Physical memory: %d%% free\n", (pm->freeFrameCount * 100) / pm->totalFrames);
+
+    for (int i = 0; i < pm->totalFrames; i++)
     {
-    case 1:
-        printf("Creating a process\n");
-        break;
-    case 2:
-        printf("Showing memory status\n");
-        break;
-    case 3:
-        printf("Showing pages table\n");
-        break;
+        printf("Frame %d: ", i);
+        if (pm->freeFrames[i])
+        {
+            printf("Free\n");
+        }
+        else
+        {
+            for (int j = 0; j < pm->frameSize; j++)
+            {
+                printf("%02X ", pm->memory[i * pm->frameSize + j]);
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+void viewPageTable(Process *process)
+{
+    printf("/n");
+    printf("Page table for process %d:\n", process->processId);
+    printf("Process size: %d bytes\n", process->size);
+    printf("Pages number: %d\n\n", process->pageTable->numPages);
+
+    for (int i = 0; i < process->pageTable->numPages; i++)
+    {
+        printf("Page %d -> Frame %d\n", i, process->pageTable->pageTable[i]);
+    }
+    printf("\n");
+}
+
+Process *createProcess(int processId, int processSize, int maxProcessSize, PhysicalMemory *pm)
+{
+    if (processSize > maxProcessSize)
+    {
+        printf("\nError: the process must be smaller than %d.\n\n", maxProcessSize);
+        return NULL;
+    }
+
+    int numPages = (processSize + pm->frameSize - 1) / pm->frameSize;
+
+    if (numPages > pm->freeFrameCount)
+    {
+        printf("Error: not enougth memory to start the process.\n\n ");
+        return NULL;
+    }
+
+    Process *process = (Process *)malloc(sizeof(Process));
+    process->processId = processId;
+    process->size = processSize;
+    process->pageTable = (PageTable *)malloc(sizeof(PageTable));
+    process->pageTable->numPages = numPages;
+    process->pageTable->pageTable = (int *)malloc(numPages * sizeof(int));
+
+    int allocatedPages = 0;
+    for (int i = 0; i < pm->totalFrames && allocatedPages < numPages; i++)
+    {
+        if (pm->freeFrames[i])
+        {
+            pm->freeFrames[i] = false;
+            pm->freeFrameCount--;
+            process->pageTable->pageTable[allocatedPages] = i;
+            allocatedPages++;
+        }
+    }
+
+    for (int i = 0; i < processSize; i++)
+    {
+        pm->memory[process->pageTable->pageTable[i / pm->frameSize] * pm->frameSize + (i % pm->frameSize)] = rand() % 256;
+    }
+
+    return process;
+}
+
+int getProcessId(int processCount, Process *processList[])
+{
+    int processId;
+    printf("Type the new process ID: ");
+    scanf("%d", &processId);
+
+    if (processId < 0)
+    {
+        printf("ID should be a number greather than 0. \n\n");
+        return getProcessId(processCount, processList);
+    };
+    for (int i = 0; i < processCount; i++)
+    {
+
+        if (processList[i]->processId == processId)
+        {
+            printf("This ID is already taken by another process!\n\n");
+            return getProcessId(processCount, processList);
+        }
+    }
+    return processId;
+}
+
+int getExistentProcessId(int processCount, Process *processList[])
+{
+    int processId;
+    bool found = false;
+    while (!found)
+    {
+        printf("Type the process ID (-1 to go back): ");
+        scanf("%d", &processId);
+        if (processId == -1)
+        {
+            break;
+        }
+
+        for (int i = 0; i < processCount; i++)
+        {
+            if (processList[i]->processId == processId)
+            {
+                found = true;
+                return processId;
+            }
+        }
+        printf("\nNo process found with the provided ID :(\n\n");
+        return getExistentProcessId(processCount, processList);
+    }
+}
+
+void showMenu(PhysicalMemory *pm, int memorySize, int processMaxSize, int pageMaxSize)
+{
+    int option;
+    Process *processList[100];
+    int processCount = 0;
+    int processId = 0;
+
+    while (true)
+    {
+        printf("The size of the physical memory is %d bytes, the processes maximun size is  %d bytes "
+               "and the pages maximum size is %d bytes.\nWhat do you want to do next?\n\n",
+               memorySize, processMaxSize, pageMaxSize);
+
+        printf("1 - Create a new process.\n");
+        printf("2 - Show memory status.\n");
+        printf("3 - Show pages table.\n");
+        printf("4 - Exit.\n");
+
+        printf("\nType the number of the action and press enter: ");
+        scanf("%d", &option);
+
+        if (option == 1)
+        {
+            int processSize;
+            processId = getProcessId(processCount, processList);
+            printf("Type the process size (bytes): ");
+            scanf("%d", &processSize);
+
+            Process *newProcess = createProcess(processId, processSize, processMaxSize, pm);
+            if (newProcess != NULL)
+            {
+                processList[processCount++] = newProcess;
+                printf("Process %d sucessfuly created.\n\n", processId);
+            }
+        }
+        else if (option == 2)
+        {
+            printf("Showing memory status...\n\n");
+            viewPhysicaMemory(pm);
+        }
+        else if (option == 3)
+        {
+            if (processCount == 0)
+            {
+                printf("\nThere are no processes in the memory! \n\n");
+                continue;
+            }
+
+            processId = getExistentProcessId(processCount, processList);
+
+            for (int i = 0; i < processCount; i++)
+            {
+                if (processList[i]->processId == processId)
+                {
+                    viewPageTable(processList[i]);
+                    break;
+                }
+            }
+        }
+        else if (option == 4)
+        {
+            printf("Exiting\n");
+            break;
+        }
+        else
+        {
+            printf("Invalid option, try again!\n");
+        }
     }
 }
 
@@ -63,6 +281,11 @@ int main()
     int pageMaxSize = checkPageSizeIsValid(readMultipleOfTwo("Input the page maximun size: "), memorySize);
     int physicalMemory[memorySize];
 
-    printf("The size of the physical memory is %d, the processes maximun size is  %d, and the pages maximum size is %d what do you thant to do next?\n\n", memorySize, processMaxSize, pageMaxSize);
-    showMenu();
+    PhysicalMemory pm;
+
+    initPhysicalMemory(&pm, memorySize, pageMaxSize);
+
+    showMenu(&pm, memorySize, processMaxSize, pageMaxSize);
+
+    freePhysicalMemory(&pm);
 }
